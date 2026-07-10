850948ab12ddf8a1fe0084bdaaa35a76200670d8
Anjali Pal <palanjali659@gmail.com>
2026-07-10 21:47:46 +0530

    Add Variables example in Java Basics

---
+++ Basics/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 21;
+        double salary = 35000.50;
+        char grade = 'A';
+        boolean isPlaced = false;
+        String name = "Anjali";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + isPlaced);
+    }
+}
