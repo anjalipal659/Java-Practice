d2625f167e6c097b37a31469bd2f287fb4696aa1
Anjali Pal <palanjali659@gmail.com>
2026-07-10 21:49:48 +0530

    Add Java primitive data types example

---
+++ Basics/DataTypes.java
@@ -0,0 +1,25 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short s = 20000;
+        int i = 100000;
+        long l = 1000000000L;
+
+        float f = 10.5f;
+        double d = 20.12345;
+
+        char c = 'J';
+
+        boolean flag = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(flag);
+    }
+}
