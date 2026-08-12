d41efac4069cf49edd6181a29df4c35ca73189ad
Anjali Pal <palanjali659@gmail.com>
2026-08-12 20:55:40 +0530

    Patterns Practice in java

---
+++ Basics/Patterns.java
@@ -0,0 +1,32 @@
+public class Patterns {
+    public static void main(String[] args) {
+
+        // Pattern 1
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Pattern 2
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Pattern 3
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
