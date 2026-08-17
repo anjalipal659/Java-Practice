9a037af23f50a0ca0281acce2d75f3649b93a0bc
Anjali Pal <palanjali659@gmail.com>
2026-08-17 23:50:53 +0530

    Add abstraction practice

---
+++ Basics/Abstraction.java
@@ -0,0 +1,42 @@
+abstract class Vehicle {
+
+    // Abstract method
+    abstract void start();
+
+    // Normal method
+    void stop() {
+        System.out.println("Vehicle stopped");
+    }
+}
+
+class Car extends Vehicle {
+
+    @Override
+    void start() {
+        System.out.println("Car starts with a key");
+    }
+}
+
+class Bike extends Vehicle {
+
+    @Override
+    void start() {
+        System.out.println("Bike starts with a button");
+    }
+}
+
+public class Abstraction {
+
+    public static void main(String[] args) {
+
+        Vehicle car = new Car();
+        car.start();
+        car.stop();
+
+        System.out.println();
+
+        Vehicle bike = new Bike();
+        bike.start();
+        bike.stop();
+    }
+}
