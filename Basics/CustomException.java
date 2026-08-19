ff0eee5d235764e5f9b2e60360180291983e7dae
Anjali Pal <palanjali659@gmail.com>
2026-08-19 21:30:10 +0530

    Add custom exception practice

---
+++ Basics/CustomException.java
@@ -0,0 +1,30 @@
+class InvalidAgeException extends Exception {
+
+    InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class CustomException {
+
+    static void checkAge(int age) throws InvalidAgeException {
+
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+
+        System.out.println("Eligible to vote");
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            checkAge(16);
+
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        System.out.println("Program continues...");
+    }
+}
