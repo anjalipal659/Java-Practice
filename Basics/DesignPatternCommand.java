18a6030963a58a6a0e4cfa6cfc1b1c8e46131ce4
Anjali Pal <palanjali659@gmail.com>
2026-09-08 22:48:18 +0530

    Add Cammand design pattern practice

---
+++ Basics/DesignPatternCommand.java
@@ -0,0 +1,81 @@
+interface Command {
+    void execute();
+}
+
+// Receiver
+class Light {
+
+    public void turnOn() {
+        System.out.println("Light is ON");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is OFF");
+    }
+}
+
+// Concrete Command
+class TurnOnCommand implements Command {
+
+    private Light light;
+
+    public TurnOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+}
+
+class TurnOffCommand implements Command {
+
+    private Light light;
+
+    public TurnOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
+
+// Invoker
+class RemoteControl {
+
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+}
+
+public class DesignPatternCommand {
+
+    public static void main(String[] args) {
+
+        Light light = new Light();
+
+        Command turnOn =
+                new TurnOnCommand(light);
+
+        Command turnOff =
+                new TurnOffCommand(light);
+
+        RemoteControl remote =
+                new RemoteControl();
+
+        remote.setCommand(turnOn);
+        remote.pressButton();
+
+        remote.setCommand(turnOff);
+        remote.pressButton();
+    }
+}
